58f51da550b8d2911ac6c8a540dd45bee85dfd1e
Justin Zelinsky <jpzelinsky@gmail.com>
2019-10-21 16:48:28 -0400

    Remove Multiplexer code

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,6 @@
 import LED from 'components/LED';
 import LEDSwitch from 'components/LEDSwitch';
-import Multiplexer from 'components/Multiplexer';
 import RotaryEncoder from 'components/RotaryEncoder';
 import SevenSegmentDisplay from 'components/SevenSegmentDisplay';
 import Switch from 'components/Switch';
-export { LED, LEDSwitch, Multiplexer, RotaryEncoder, SevenSegmentDisplay, Switch };
+export { LED, LEDSwitch, RotaryEncoder, SevenSegmentDisplay, Switch };
